44178c4b3216a396c97b6e1e15715eecb6e930cf
Andrea Benfatti <welldone@welldone-think.localdomain>
2019-07-10 15:12:37 +0200

    use kalman filter

--- marlin/HeadingSensor.py
+++ marlin/HeadingSensor.py
@@ -2,7 +2,7 @@ import logging
 import numpy as np 
 import time
 from threading import Thread
-from marlin.utils import headingToVector
+from marlin.utils import headingToVector, KalmanFilter
 from marlin.Provider import Provider
 
 
@@ -11,9 +11,44 @@ class HeadingSensor:
         self.logger = logging.getLogger(__name__)
         self.GPS = Provider().get_GPS()
         self.APS = Provider().get_AbsolutePositionSensor()
-        self.heading = 0
+
+        F = np.array([[1]])
+        B = np.array([[1]])
+        H = np.array([[1]])
+        Q = np.array([[0.01]]) # process noise covariance, and avoid P equal zero
+        R = np.array([[10]]) # measurament covariance 
+        P = np.array([[0.1]]) # process noise
+        self.kf = KalmanFilter(F=F, B=B, H=H, Q=Q, R=R, P=P)
+        self.old_heading = self.APS.state['heading']
+        self.heading_setup = False
 
 
     def get_state(self):
-        heading = self.APS.state['heading']
+        new_heading = self.APS.state['heading']
+
+        # setup initial heading
+        if not self.heading_setup and self.APS.state['mag_cal'] == 3:
+            self.kf.x = np.array([[new_heading]])
+            self.heading_setup = True
+            self.old_heading = new_heading
+            return new_heading
+
+        # calculate heading using kalman filter
+        heading = self.kf.predict(new_heading - self.old_heading)[0][0]
+        
+        # normalize heading
+        heading = heading % 360
+        self.kf.x = np.array([[heading]])
+
+
+        # adjust kalman filter using GPS heading if boat is moving
+        if self.GPS.state['speed'] > 1.0:
+            gps_heading = self.GPS.state['heading']
+            if gps_heading - heading > 180:
+                gps_heading -= 360
+            if gps_heading - heading < -180:
+                gps_heading += 360
+
+            self.kf.update(np.array([[self.GPS.state['heading']]]))
+
         return heading 
