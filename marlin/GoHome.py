73a93f65b9f1e7c5819459ea2df86988fe4e9c93
Andrea Benfatti <WellDone2094@gmail.com>
2019-07-10 15:35:14 +0200

    update GoHome with new autonomy algorithm

--- marlin/GoHome.py
+++ marlin/GoHome.py
@@ -1,5 +1,6 @@
 from threading import Thread
 from collections import defaultdict
+from simple_pid import PID
 from marlin.Provider import Provider
 from marlin.Astar import Node, A_star
 from marlin.utils import headingToVector, directionError, clip
@@ -13,7 +14,7 @@ class GoHome:
     UDPDATE_TIME_S = 1
     AUTOMATIC_START_S = 60
 
-    def __init__(self):
+    def __init__(self, min_distance = 2):
         self.logger = logging.getLogger(__name__)
         self.grid_map = defaultdict(defaultdict(lambda: None))
         self.home = None
@@ -21,13 +22,15 @@ class GoHome:
         self.GPS = Provider().get_GPS()
         self.autonomy = Provider().get_Autonomy()
         self.RC = Provider().get_RC()
+        self.pid_controller = PID(*self.pid)
         self.path = None
         self.off_timestamp = time.time()
+        self.next_target = 0
+        self.min_distance = min_distance
+        self.stop = False
 
         self.loop_thread = Thread(target=self.update_loop)
-        self.stop = False
         self.loop_thread.start()
-        self.path = None
 
     def update_loop(self):
         while not self.stop:
@@ -54,6 +57,7 @@ class GoHome:
         if now - self.off_timestamp < self.AUTOMATIC_START_S:
             return
 
+        self.next_target = 0
         self.path = self.calculate_path().node_list
         self.running = True
 
@@ -68,39 +72,52 @@ class GoHome:
     def is_active(self):
         return self.running
 
-    def stop_autonomy(self):
+    def stop(self):
         self.is_running = False
         self.path = None
+        self.next_target = 0
         self.off_timestamp = time.time()
 
+    def start(self):
+        if self.running:
+            return
+
+        self.next_target = 0
+        self.path = self.calculate_path().node_list
+        self.running = True
+
+
     def get_state(self):
+        # if not running or reached last point
         boat_position = utm.from_latlon(self.GPS.state['lat'],
                                         self.GPS.state['lng'])[:2]
-        rounded_position = (round(boat_position[0]), round(boat_position[1]))
 
-        # if next point is close the boat, reapeat this with successive point
+        # select next waypoint
         while True:
             # check that boat is running and there are point left
-            if not self.is_running or self.path is None or len(self.path) == 0:
+            if not self.is_running or self.path is None or self.next_target >= len(self.path):
                 self.logger.info('Last waypoint reached')
-                self.stop_autonomy()
+                self.stop()
                 return {'trust': 0, 'turn': 0, 'scale': 0}
 
-            if self.path[0] == Node(*rounded_position):
-                self.path.remove(self.path[0])
-                continue
+            # check if we reached waypoint
+            waypoint = self.path[self.next_target]
+            if pointDistance(boat_position, waypoint) > self.min_distance:
+                break
 
-            target_position = self.path[0]
+            self.next_target += 1
+            self.logger.debug('Reached target {}'.format(self.next_target-1))
+
+        target_position = self.path[self.next_target]
 
         boat_direction = headingToVector(self.APS.state['heading'])
-        self.logger.debug(
-            'position: {} direction: {} waypoint: {}'.format(
-                boat_position, boat_direction, target_position))
+        #boat_direction = self.heading_sensor.get_state()['vector_heading']
 
         error = directionError(boat_position, target_position, boat_direction)
         correction = self.pid_controller(error)
 
         # if boat is point in opposite direction turn on the spot
-        trust = 500 if abs(error) < 1 else 0
+        trust = 500*(1.0-error) if abs(error) < 0.5 else 0
+        trust = trust * self.speed/100.0
         turn = 500 * clip(correction, -1, 1)
-        return {'trust': trust, 'turn': turn, 'scale': self.speed/100}
+        return {'trust': trust, 'turn': turn, 'scale': 1}
